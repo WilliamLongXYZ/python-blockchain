2dd8433f7fa6eb86c0dcfde56de8ca9cae1aa87c
William Long <will@williamlong.xyz>
2021-10-22 19:53:14 +0000

    Created new blockchain storage, and began editing main.py to be more clean and a better blockchain.

--- src/genesis.py
+++ src/genesis.py
@@ -1,34 +1,49 @@
+import datetime
 import hashlib
 import os
+from socket import gethostbyname, gethostname
 
-block_num = 0
-data = "Genesis"
-nonce = 0
-previous_hash = 0x0
-
-def hash():
-    h = hashlib.sha512()
-    h.update(
-    str(nonce).encode('utf-8') +
-    str(data).encode('utf-8') +
-    str(previous_hash).encode('utf-8') +
-    str(block_num).encode('utf-8') +
-    str(key).encode('utf-8')
-    )
-    return h.hexdigest()
-
-key = os.environ['crypt-key']
 
 class Block:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        self.timestamp = datetime.datetime.now()
+        self.nonce = 0
+        self.data = "Genesis"
+        self.previous_hash = 0x0
+        self.node_number_formatted = 0000
+        self.node_ip = gethostbyname(gethostname())
+        self.block_num = 0
+        self.key = os.environ['crypt-key']
+        self.hash = self.hash()
+        self.block_num = 0
+        self.data = "Gensis"
+        self.nonce = 0
 
     def __repr__(self):
-        return (
-        f'Block Hash: {str(hash())}\n'
-        f'Block #: {block_num}\n'
-        f'Block Data: {data}\n'
-        f'Hashes: {nonce}\n'
-        )
+        return (f"Block Hash: {str(self.hash)}\n"
+                f"Block #: {self.block_num}\n"
+                f"Block Data: {self.data}\n"
+                f"Mined From Node: {self.node_number_formatted}\n"
+                f"Mined: {self.timestamp}\n"
+                f"Hashes: {self.nonce}\n")
 
-print(Block("Gensis"))
+    def hash(self):
+        h = hashlib.sha512()
+        h.update(
+        str(self.nonce).encode('utf-8') +
+        str(self.data).encode('utf-8') +
+        str(self.previous_hash).encode('utf-8') +
+        str(self.node_number_formatted).encode('utf-8') +
+        str(self.node_ip).encode('utf-8') +
+        str(self.timestamp).encode('utf-8') +
+        str(self.block_num).encode('utf-8') +
+        str(self.key).encode('utf-8')
+        )
+        return h.hexdigest()
+
+
+def main():
+    with open("blockchain.raw", 'a') as rawstore:
+        rawstore.write(f"{Block()}\n")
+
+main()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import datetime
 import hashlib
+import socket
 
 
 class Block:
@@ -8,9 +9,9 @@ class Block:
     hash = None
     nonce = 0
     previous_hash = 0xf6837146c8e17306ffc692e36eaeeb581ef04d0d549af66a65fca7257ffab86b4bf0b181dd12b66547c170e1f0771327e8a25903b48d90f61831455e45142409
-    timestamp = datetime.datetime.now
-    pc_num_for = format(0000, "04b")
-    pc_ip = "172.18.0.164"
+    timestamp = datetime.datetime.now()
+    node_number_formatted = format(0000, "04b")
+    node_ip = socket.gethostbyname(socket.gethostname())
 
     def __init__(self, data):
         self.data = data
@@ -20,24 +21,25 @@ class Block:
         h.update(
             str(self.nonce).encode('utf-8') + str(self.data).encode('utf-8') +
             str(self.previous_hash).encode('utf-8') +
-            str(self.pc_num_for).encode('utf-8') +
-            str(self.pc_ip).encode('utf-8') +
-            # str(self.timestamp).encode('utf-8') +
+            str(self.node_number_formatted).encode('utf-8') +
+            str(self.node_ip).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') +
             str(self.block_num).encode('utf-8'))
         return h.hexdigest()
 
     def __repr__(self):
-        return (f'Block Hash: {str(self.hash())}\n'
-                f'Block #: {self.block_num}\n'
-                f'Block Data: {self.data}\n'
-                f'Hashes: {self.nonce}\n')
+        return (f"Block Hash: {str(self.hash())}\n"
+                f"Block #: {self.block_num}\n"
+                f"Block Data: {self.data}\n"
+                f"Hashes: {self.nonce}\n")
 
 
 class Blockchain:
+    data = "Data from 1"
     difficulty = 32
     max_nonce = 2**32
     target = 2**(512 - difficulty)
-    block = Block(None)
+    block = Block(data)
     dummy = head = block
 
     def add(self, block):
@@ -55,25 +57,28 @@ class Blockchain:
                 block.nonce += 1
 
     def show(self):
-        if not self.head:
-            self.head = self.block
+        if not self.head: self.head = self.block
         while self.head:
             print(self.head)
+            self.write_block()
             self.head = self.head.next
+    def write_block(self):
+        with open("blockchain.raw", 'a') as rawstore:
+            rawstore.write(f"{self.head}\n")
 
+def mine(blockchain, data):
+    blockchain.mine(data)
+    blockchain.show()
 
-def _main():
+def read_block():
+    pass
+
+def main():
     blockchain = Blockchain()
     # mining
     for n in range(5):
-        blockchain.mine(Block(f"Block {n+2}"))
-        blockchain.show()
-    #adding
-    blockchain.add(Block('added block'))
-    blockchain.show()
-    blockchain.add(Block('added block2'))
-    blockchain.show()
+        mine(blockchain, Block(f"Data from {n+1}"))
 
 
 if __name__ == "__main__":
-    _main()
+    main()
